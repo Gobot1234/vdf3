caf17266448bf572e65687568ad63c69eb96afe2
James <gobot1234yt@gmail.com>
2020-10-17 18:20:34 +0100

    Improve VDFDict

--- vdf/vdf_dict.py
+++ vdf/vdf_dict.py
@@ -14,12 +14,11 @@ _VT = TypeVar("_VT")
 
 
 class VDFDict(MultiDict[_VT], Generic[_VT]):
-    """A dictionary that supports duplicate keys."""
-    def __str__(self):
-        return repr(self)
-
+    """A dictionary that supports duplicate keys. Bases MultiDict"""
     def __repr__(self):
         return f"{self.__class__.__name__}({list(self.items())})"
+
+    __str__ = __repr__
 
     def copy(self) -> VDFDict[_VT]:  # MultiDict.copy doesn't seem to use self.__class__
         return self.__class__(self.items())
